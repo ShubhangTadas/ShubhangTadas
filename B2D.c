52bc493001a0c0f09d3d919e389d8efe890a4e2d
Shubhang Tadas <101917582+ShubhangTadas@users.noreply.github.com>
2022-03-19 12:31:27 +0530

    Add files via upload

---
+++ B2D.c
@@ -0,0 +1,35 @@
+ #include<stdio.h>
+ 
+ 
+ int btod(int);		// Prototype of btod() function
+ int main()
+ {
+ 	int binary, decimal;
+ 	// Accept input in binary format
+ 	printf("Enter binary input :");
+ 	scanf("%d", &binary);
+ 	if(binary < 0)
+ 	{
+ 		printf("Invalid Input. \n");
+ 		return 1;
+ 	}
+ 	// Invoke botd() to convert binary to decimal
+ 	decimal = btod(binary);
+ 	
+ 	// Print the decimal equivalent of binary
+ 	printf("Decimal equivalent = %d\n", decimal);
+ 	
+ 	return 0;
+ }
+ 
+ int btod(int bin)
+ {
+ 	if (bin == 0)
+ 	{
+ 		return 0;
+ 	}
+ 	else
+ 	{
+ 		return (bin % 10 + btod(bin/10) * 2);
+ 	}
+ }
--- MM.c
+++ MM.c
@@ -1,25 +1,89 @@
 #include<stdio.h>
-int main ()
 
+int main()
 {
-int m,n,p,q,i,j,k;
-printf("Enter size of matrix A");
-scanf("%d%d, &m,&n");
-printf("Enter size of matix B");
-scanf("%d%d", &p,&q);
-if (n!=p)
+	int a[10][10],b[10][10],c[10][10];
+	int m,n,p,q;
+	int i,j,k;
+	
+	// Input the order of Matrix A - m x n
+	printf("Enter the order of matrix A :");
+	scanf("%d%d",&m,&n);
+	
+	// Input the order of Matrix B - p x q
+	printf("Enter the order of matrix B:");
+	scanf("%d%d",&p,&q);
+	
+	/* For multiplication of two matrices, the 
+	   number of columns in the first matrix 
+	   should be equal to the number of rows 
+	   in the second matrix */
+	if(n!=p)
 	{
-	printf("Matrix multiplication not possible");
-	return(1);
-	}
-int a[m][n], b[p][q], c[m][q];
-printf("Insert data into matrix A");
-for (i=0;i<n;i++)
-	{
-		printf"(%d\t", a[i][j]);
+		printf("Number of columns of Matrix A is not equal to number of rows of matrix B\n");
+		printf("Matrix Multiplication not possible....\n");
+		return (1);
 	}
 	
-		
-
+	// Input the elements into Matrix A
+	printf("\nEnter %d elements into matrix A : ", m*n);
+	for(i=0;i<m;i++)
+		for(j=0;j<n;j++)
+			scanf("%d",&a[i][j]);
+	
+	// Display matrix A in matrix format
+	printf("\nThe  matrix A is ---\n");
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}	
+	
+	// Input the elements into Matrix B
+	printf("\nEnter %d elements into matrix B : ", p*q);
+	for (i=0;i<p;i++)
+		for (j=0;j<q;j++) 
+			scanf("%d",&b[i][j]);
+	
+	// Display Matrix B in matrix format		
+	printf("\nThe matrix B is ---\n");
+	for(i=0;i<p;i++)
+	{
+		for(j=0;j<q;j++)
+		{
+			printf("%d\t",b[i][j]);
+		}
+		printf("\n");
+	}
+	
+	// Compute (Matrix A) X (Matrix B)
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<q;j++)
+		{
+			c[i][j] = 0;
+			for(k=0;k<n;k++)
+			{
+				c[i][j] = c[i][j] + (a[i][k] * b[k][j]);
+			}
+		}
+	}
+	
+	// Display product matrix - Matrix C
+	printf("\nThe product matrix is ---\n\n");
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<q;j++)
+		{
+			printf("%d\t",c[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
 
 
